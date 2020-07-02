3856d463ad53df5c7095fad176be11ba0d856179
Jimi Friis <jimi.friis@gmail.com>
2020-07-03 01:38:53 +0200

    add a start script - eventstore start command

--- eventstore start command.ps1
+++ eventstore start command.ps1
@@ -1,1 +1,1 @@
-EventStore.ClusterNode.exe --db ./db --log ./logs --config=config.yaml --enable-atom-pub-over-http --run-projections=all --start-standard-projections=true
+EventStore.ClusterNode.exe --db ./db --log ./logs --config=config.yaml --enable-atom-pub-over-http --enable-external-tcp --run-projections=all --start-standard-projections=true
